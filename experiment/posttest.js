5bd5ad7711bb84619abcef9291d4fc9dbb9979a5
IITGVLAB <75976828+IITGVLAB@users.noreply.github.com>
2021-02-24 12:22:18 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -89,21 +89,25 @@
 
 
     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1.What is the percentage of whorls in Caucasoid?", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "30 to 40", ///// Write the option 1 inside double quotes
+                b: "40 to 50", ///// Write the option 2 inside double quotes
+                c: "10 to 20",
+                d: "20 to 30"
             },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
+            correctAnswer: "d" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "The __________ are more concerned in establishing variations in respect of traits among different human populations.",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
+        a: "Anthropologist",                  ///// Write the option 1 inside double quotes
+        b: "Archeologist",                  ///// Write the option 2 inside double quotes
+        c: "Dermatologist",  
+        d: "Geographical Statistician"     
+      },
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
     },
 
 
